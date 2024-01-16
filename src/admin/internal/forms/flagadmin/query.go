1ecd59369b05171ca1ff64de437ca50f86e959b8
Mehdi Mostafavi <56168370+MehdiMstv@users.noreply.github.com>
2024-01-16 14:50:19 +0330

    Add type conversion for flags (#7)

--- src/admin/internal/forms/flagadmin/query.go
+++ src/admin/internal/forms/flagadmin/query.go
@@ -1,14 +1,17 @@
 package flagadmin
 
 import (
+	"fmt"
 	"github.com/GoAdminGroup/go-admin/modules/db"
 	"github.com/gin-gonic/gin"
+	"strconv"
+	"strings"
 )
 
 func GetFlagsByService(c *gin.Context, conn db.Connection) {
 	serviceName := c.Request.FormValue("service_name")
 
-	rows, err := conn.Query("SELECT name, value FROM flags WHERE service_name = ?", serviceName)
+	rows, err := conn.Query("SELECT name, value, type FROM flags WHERE service_name = ?", serviceName)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
@@ -18,7 +21,33 @@ func GetFlagsByService(c *gin.Context, conn db.Connection) {
 
 	// Iterate through the results and parse values
 	for _, v := range rows {
-		flags[v["name"].(string)] = map[string]string{"value": v["value"].(string)}
+		fmt.Println(v)
+		stringValue := v["value"].(string)
+		stringName := v["name"].(string)
+		switch v["type"].(int64) {
+		case 0:
+			flags[stringName] = stringValue
+		case 1:
+			if strings.ToLower(stringValue) == "true" {
+				flags[stringName] = true
+				continue
+			}
+			if strings.ToLower(stringValue) == "false" {
+				flags[stringName] = false
+				continue
+			}
+			c.JSON(500, gin.H{"Error": fmt.Sprintf("Invalid boolean flag for flag name: %s, value: %s", stringName, stringValue)})
+			return
+		case 2:
+			flags[stringName], err = strconv.Atoi(stringValue)
+			if err != nil {
+				c.JSON(500, gin.H{"Error": fmt.Sprintf("Invalid integer flag for flag name: %s, value: %s", stringName, stringValue)})
+				return
+			}
+		default:
+			c.JSON(400, gin.H{"Error": "Bad request!"})
+		}
+
 	}
 
 	c.JSON(200, flags)
