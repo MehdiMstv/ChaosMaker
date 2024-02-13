be2822e00e7d434cde7b757db69693520c8d930c
Mehdi Mostafavi <mehdi.mostafavi7900@gmail.com>
2024-02-13 22:47:13 +0330

    Refactor admin apis

--- src/admin/internal/forms/chaosadmin/api.go
+++ src/admin/internal/forms/chaosadmin/api.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"github.com/GoAdminGroup/go-admin/modules/db"
 	"github.com/gin-gonic/gin"
+	"io"
 )
 
 func SetChaosStatus(c *gin.Context, conn db.Connection) {
-	chaosID := c.Request.FormValue("chaos_id")
-	fmt.Print(chaosID)
+	chaosID := c.Request.FormValue("id")
 	row, err := conn.Query("SELECT status FROM chaos WHERE id = ?", chaosID)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
@@ -18,13 +18,19 @@ func SetChaosStatus(c *gin.Context, conn db.Connection) {
 	chaosStatus := row[0]["status"].(string)
 	switch chaosStatus {
 	case "Unknown":
-		chaosStatus = "Starting"
-	case "Starting":
 		chaosStatus = "Getting Requests"
 	case "Getting Requests":
 		chaosStatus = "Invoking Requests"
 	case "Invoking Requests":
 		chaosStatus = "Done"
+		chaosResult, _ := getResult(c)
+		fmt.Println(chaosResult)
+		_, err = conn.Query("UPDATE chaos SET status=?, result=? WHERE id=?", chaosStatus, chaosResult, chaosID)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(200, gin.H{})
 	default:
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
@@ -36,4 +42,13 @@ func SetChaosStatus(c *gin.Context, conn db.Connection) {
 	}
 
 	c.JSON(200, gin.H{})
+}
+
+func getResult(c *gin.Context) (string, error) {
+	jsonData, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(string(jsonData))
+	return string(jsonData), nil
 }
